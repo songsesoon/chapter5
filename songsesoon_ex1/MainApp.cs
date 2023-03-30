b80518a22b2b1a43efc0b862830779d8014a5efa
세순 송 <sss010201sss@naver.com>
2023-03-30 14:15:26 +0900

    초기 커밋.

---
+++ songsesoon_ex1/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace songsesoon_ex1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
