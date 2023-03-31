3552285abd4b35adc633e4a0ee99adc752cf3a2b
세순 송 <sss010201sss@naver.com>
2023-03-31 16:14:40 +0900

    2023.03.31.4.00

--- songsesoon_ex4/MainApp.cs
+++ songsesoon_ex4/MainApp.cs
@@ -6,6 +6,28 @@ namespace songsesoon_ex4
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("반복 횟수를 입력하세요: ");
+                int ioop_input = int.Parse(Console.ReadLine());
+
+                if (ioop_input <= 0)
+                {
+                    Console.WriteLine("0보다 작거나 같은 수를 입력할 수는 없습니다.\n");
+                    continue;
+                }
+                else
+                {
+                    for (int i = 0; i < ioop_input; ++i)
+                    {
+                        for (int j = 0; j <= i; ++j)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
         }
     }
-} 
+}
