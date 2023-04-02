dec9aad4a689586fdfe4f4934b61eab45bb67ea1
세순 송 <sss010201sss@naver.com>
2023-04-03 08:46:44 +0900

    2023.04.03

---
+++ songsesoon_ex3-2/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace songsesoon_ex3
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            int external_loop = 1;
+            int? internal_loop = null;
+
+            do 
+            {
+                internal_loop = 0;
+
+                do 
+                {
+                    if (internal_loop == 6 - external_loop)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("*");
+                    internal_loop++;
+                } while (true) ;
+                Console.WriteLine();
+                external_loop++;
+            } while (external_loop <= 5) ;
+        }
+    }
+}
